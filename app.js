ebe9cec2fa37cc37e126d1c18bc59ca06f498be1
Priscila Cabral <priscilacabral.simao@gmail.com>
2020-06-24 17:19:07 -0300

    Arrumando o problema de Cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let apiKey = {
     key: '507b31ce-e315-46d0-8382-4fe802d4006a'
 }
 
-fetch('http://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + apiKey.key).then((response) => {
+fetch('https://cors-anywhere.herokuapp.com/pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + apiKey.key).then((response) => {
     if(!response.ok) throw new Error('Erro ao executar a requisição de status ' + response.status);
     return response.json();
 }).then((api) => {
