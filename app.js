09638bccd4d2836e68e79376cf3dd4ef2982b8e2
Priscila Cabral <priscilacabral.simao@gmail.com>
2020-06-24 17:58:20 -0300

    Inserido o nome e os símbolos das criptomoedas

--- app.js
+++ app.js
@@ -6,7 +6,30 @@ fetch('https://cors-anywhere.herokuapp.com/pro-api.coinmarketcap.com/v1/cryptocu
     if(!response.ok) throw new Error('Erro ao executar a requisição de status ' + response.status);
     return response.json();
 }).then((api) => {
-    console.log(api)
+    var texto = "";
+    for(let i = 0; i < 10; i++){
+        texto = texto + 
+        `
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            <div class="col s2">
+              <img src="./img/coin.png" alt="" class="circle responsive-img" width="150">
+            </div>
+            <div class="col s10">
+              <span class="black-text">
+                ${api.data[i].name}
+              </span>
+              <p>${api.data[i].symbol}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+            
+        `;
+
+        document.getElementById("cripto").innerHTML = texto;
+    }
 }).catch((error) => {
     console.log(error.message)
 });
